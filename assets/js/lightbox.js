d0c2031bd731a14fb371fdb96fc03f49ac413628
borough11 <stevegeall@gmail.com>
2024-11-10 22:04:36 +1300

    modified:   assets/js/lightbox.js

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -150,7 +150,7 @@ document.addEventListener("DOMContentLoaded", function() {
     elements.forEach(element => {
         element.addEventListener("click", function(event) {
             event.preventDefault();
-            document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="img" style="background: url(\''+this.getAttribute('href')+'\') center center / contain no-repeat;" title="'+this.getAttribute('title')+'" ><img src="'+this.getAttribute('href')+'" alt="'+this.getAttribute('title')+'" /><div style="margin-top:40px; max-width: fit-content; margin-left: auto; margin-right: auto;">' + this.getAttribute("title") + '</div></div><span>'+this.getAttribute('title')+'</span>';
+            document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="img" style="background: url(\''+this.getAttribute('href')+'\') center center / contain no-repeat;" title="'+this.getAttribute('title')+'" ><img src="'+this.getAttribute('href')+'" alt="'+this.getAttribute('title')+'" /><div style="margin-top:45px; margin-left: auto; margin-right: auto;">' + this.getAttribute("title") + '</div></div><span>'+this.getAttribute('title')+'</span>';
             document.getElementById('lightbox').style.display = 'block';
 
             setGallery(this);
